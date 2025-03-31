53815ec520901c91f8d8ff7d2db39c350c148331
ilegitt <ilegithub@gmail.com>
2025-04-01 01:36:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
+
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
@@ -13,6 +14,7 @@ resource "aws_subnet" "myapp-subnet-1" {
   vpc_id            = aws_vpc.myapp-vpc.id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
+
   tags = {
     Name = "${var.env_prefix}-subnet-1"
   }
@@ -20,24 +22,26 @@ resource "aws_subnet" "myapp-subnet-1" {
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
+
   tags = {
     Name = "${var.env_prefix}-igw"
   }
 }
 
 resource "aws_default_route_table" "main-rtb" {
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  default_route_table_id = aws_route_table.myapp-rtb.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
   }
+
   tags = {
     Name = "${var.env_prefix}-main-rtb"
   }
 }
 
-resource "aws_default_security_group" "default-sg" {
+resource "aws_security_group" "custom-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
@@ -51,15 +55,14 @@ resource "aws_default_security_group" "default-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ip_range]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = []
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.allowed_ip_range]
   }
 
   tags = {
@@ -70,10 +73,12 @@ resource "aws_default_security_group" "default-sg" {
 data "aws_ami" "latest-amazon-linux-image" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
     name   = "name"
     values = ["amzn2-ami-kernel-*-x86_64-gp2"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -105,26 +110,56 @@ resource "aws_instance" "myapp-server" {
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone      = var.avail_zone
   associate_public_ip_address = true
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name               = aws_key_pair.ssh-key.key_name
 
   user_data = <<-EOF
     #!/bin/bash
-    sudo yum update -y && sudo yum install -y docker
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo usermod -aG docker ec2-user
+    yum update -y
+    amazon-linux-extras install docker
+    service docker start
+    usermod -a -G docker ec2-user
+    systemctl enable docker
 
-    sudo curl -L "https://github.com/docker/compose/releases/download/2.5.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+
+    yum install -y aws-cli
+    aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin <aws_account_id>.dkr.ecr.eu-north-1.amazonaws.com
+
+    mkdir /home/ec2-user/app
+    cd /home/ec2-user/app
+
+    cat <<EOL > docker-compose.yml
+    version: '3.8'
+    services:
+      web:
+        image: <aws_account_id>.dkr.ecr.us-east-1.amazonaws.com/my-app:latest
+        ports:
+          - '8080:8080'
+        environment:
+          - POSTGRES_HOST=postgres
+          - POSTGRES_DB=mydatabase
+          - POSTGRES_USER=myuser
+          - POSTGRES_PASSWORD=mypassword
+      postgres:
+        image: postgres:latest
+        environment:
+          POSTGRES_USER: myuser
+          POSTGRES_PASSWORD: mypassword
+          POSTGRES_DB: mydatabase
+        ports:
+          - '5432:5432'
+        volumes:
+          - postgres_data:/var/lib/postgresql/data
+
+    volumes:
+      postgres_data:
+    EOL
+
+    if command -v docker-compose &> /dev/null; then docker-compose up -d; else echo "Docker Compose not found, installation might have failed."; fi
   EOF
 
   tags = {
     Name = "${var.env_prefix}-server"
   }
-}
-
-output "private_key" {
-  value     = tls_private_key.my_key.private_key_pem
-  sensitive = true
 }
